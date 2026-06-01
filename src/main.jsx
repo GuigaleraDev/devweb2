765665433599e741fa0e155263d87cdb45948518
GuigaleraDev <galeracunha@hotmail.com>
2026-05-31 21:01:50 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createHead, UnheadProvider } from '@unhead/react/client'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import App from './App.jsx' // Alterado de .tsx para .jsx
+import App from './App.jsx'
 import './index.css'
 
 const head = createHead()
